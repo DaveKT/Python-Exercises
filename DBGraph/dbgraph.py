b80922adb2957badabf6efc84a41b85b71be47cd
David Tassara <phi.fibonacci@gmail.com>
2017-01-30 10:14:05 -0800

    Updated the SQLDB Example

--- DBGraph/dbgraph.py
+++ DBGraph/dbgraph.py
@@ -1,20 +1,30 @@
 #!/usr/local/bin/python3
 
-import argparse
 import sqlite3
-import re
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from dateutil import parser
+from matplotlib import style
+style.use('seaborn-bright')
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("db", help="The name of the DB to connect to")
-args=parser.parse_args()
-
-conn = sqlite3.connect(args.db)
+conn = sqlite3.connect("TestSQLiteDB.db")
 c = conn.cursor()
 
-tables = []
+dates = []
+values = []
 
-for row in c.execute("""SELECT tbl_name FROM sqlite_master WHERE type = 'table'"""):
-	tables.append(row)
+for row in c.execute("Select id, value from data where id > 23"):
+	dates.append(row[0])
+	values.append(row[1])
 
-print(tables)
+c.close
+conn.close()
+
+plt.xkcd()
+plt.title('Whoo  Hoo!!!')
+plt.ylabel("Happiness")
+plt.xlabel("Time")
+plt.xticks([])
+plt.annotate("Moment I realized I\ncould make XKCD\ncharts!", xy=(27.8,1.2), xytext=(25,3), arrowprops=dict(arrowstyle='-'))
+plt.plot(dates,values)
+plt.show()
---
+++ DBGraph/dbgraph_readme.md
@@ -0,0 +1,15 @@
+
+##Purpose
+Queries a sqlite db and plots returned values in XKCD style using matplotlib
+
+usage: dbgraph.py
+
+###Sample Output
+
+![](Happiness.png)
+
+###Additional Reading for XKCD Graphs
+
+*   [http://matplotlib.org/xkcd/examples/showcase/xkcd.html](http://matplotlib.org/xkcd/examples/showcase/xkcd.html)
+*   [http://matplotlib.org/users/annotations_intro.html](http://matplotlib.org/users/annotations_intro.html)
+*   [http://jakevdp.github.io/blog/2013/07/10/XKCD-plots-in-matplotlib/](http://jakevdp.github.io/blog/2013/07/10/XKCD-plots-in-matplotlib/)
