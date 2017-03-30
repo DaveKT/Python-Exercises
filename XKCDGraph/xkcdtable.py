34d9bf83509fb5cc481f527998ce8dc32cfcbe5d
David Tassara <phi.fibonacci@gmail.com>
2017-03-30 16:05:15 -0700

    Updated matplotlib table examples.

--- XKCDGraph/xkcdtable.py
+++ XKCDGraph/xkcdtable.py
@@ -9,15 +9,14 @@ rlabels = ['Page Views']
 values = [[185, 133, 115, 0, 0, 70, 165, 47, 113, 82, 0, 0, 90, 161, 187]]
 
 plt.xkcd()
+plt.figure(figsize=(10,3))
 plt.axes(frameon=False)
 plt.xticks([])
 plt.yticks([])
-
-plt.table(cellText=values,
-          colWidths=[0.1] * 15,
-          rowLabels=rlabels,
-          colLabels=clabels,
-          loc='center')
-
-
+dataTable = plt.table(cellText=values,
+                      colWidths=[0.05] * 15,
+                      rowLabels=rlabels,
+                      colLabels=clabels,
+                      loc='center')
+dataTable.scale(1,2)
 plt.show()
